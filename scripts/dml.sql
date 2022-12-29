16df94b710da723235417ef71f31c0b967b6f054
MateusCaltabiano <mateus.frauzino@gmail.com>
2022-12-29 18:25:18 -0300

    populando tabelas local e jogador

--- scripts/dml.sql
+++ scripts/dml.sql
@@ -1,3 +1,10 @@
 insert into nivel (xpnecessario, forca, defesa)
 	values (10, 10, 10), (20, 20, 20), (30, 30, 30), (40, 40, 40), (50, 50, 50), (60, 60, 60), 
 		   (70, 70, 70), (80, 80, 80), (90, 90, 90)
+
+insert into localtab (descricao, nome)
+	values ('descricao do local', 'planicie'), ('descricao do local', 'montanha'),
+		   ('descricao do local', 'cidade');
+
+insert into jogador (nome, vidaatual, vidatotal, experiencia, forca, defesa, id_local, id_nivel)
+	values ('dummy', 100, 100, 5, 10, 10, 1, 1)
