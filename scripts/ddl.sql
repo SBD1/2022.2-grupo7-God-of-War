22e8188e7277a5db1189d75d5e0a52921285d358
MateusCaltabiano <mateus.frauzino@gmail.com>
2022-12-28 22:49:07 -0300

    adição da tabela npc

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -3,3 +3,13 @@ create table localTab(
 	descricao varchar(512),
 	nome varchar(512)
 );
+
+create table npc(
+	idNPC serial constraint pk_idNPC primary key,
+	nome varchar(512),
+	tipo varchar(512),
+	descricao varchar(512),
+	pos int,
+	dialogo varchar(512),
+	constraint fk_idLocal foreign key (pos) references localTab(id)
+);
